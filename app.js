847ecf846b4e4d39919e91bb493fd772f4e80bb7
Raj Shakya <119093677+Rajshakya0101@users.noreply.github.com>
2024-07-26 16:11:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,35 +9,8 @@
     });
 })();
 
-'speechSynthesis' in window ? console.log("Web Speech API supported!") : console.log("Web Speech API not supported :-(")
-
-
 const quote = document.getElementById('quote')
 const author = document.getElementById('author')
-
-// const url = 'https://quotes15.p.rapidapi.com/quotes/random/?language_code=en';
-// const options = {
-//     method: 'GET',
-// 	headers: {
-// 		'x-rapidapi-key': '299676854cmshb9fb7457875145ep1c3300jsna4b5564a02ea',
-// 		'x-rapidapi-host': 'quotes15.p.rapidapi.com'
-// 	}
-// };
-// // document.getElementById('new-quote').addEventListener("click", getQuote(url))
-
-// async function getQuote (url) {
-//     try {
-//         const response = await fetch(url, options);
-//         const result = await response.json();
-//         quote.innerHTML = `"${result.content}"`
-//         author.innerHTML = `--- by ${result.originator.name}`
-//         console.log(result);
-//     } catch (error) {
-//         console.error(error);
-//     }
-    
-// }
-
 
 const text_to_speech = () => {
     const quoteText = quote.innerHTML
@@ -54,8 +27,6 @@ const text_to_speech = () => {
             msg.lang = "en-US"
             msg.rate = 0.7
             synth.speak(msg);
-            // demo.addEventListener("click", text_to_speech)
-            // demo.disabled = false;
         }
     }
     else{
@@ -63,7 +34,6 @@ const text_to_speech = () => {
             buttons: false,
             timer: 1500,
             icon: "error",
-            // className: "black-bg"
         });
     }
 
@@ -72,7 +42,6 @@ const text_to_speech = () => {
 document.getElementById('speaker').addEventListener('click', text_to_speech)
 
 function tweet(){
-    // console.log(quote.innerHTML)
     if(quote.innerHTML){
         window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "     " + author.innerHTML, "Tweet Window", "width=600", "height=300")
     }
@@ -100,23 +69,17 @@ const copyQuote = async () => {
                 buttons: false,
                 timer: 3000,
                 icon: "success",
-                // className: "black-bg"
             });
         } catch (err) {
             console.error('Failed to copy: ', err);
             swal("Failed to copy quote!", "There was an error copying the quote.", "error");
         }
-        // // console.log(combined.select())
-        // combined.select();
-        // document.execCommand("copy");
-        // swal("Quote Copied!", combined, "success");
     }
     else{
         swal("Failed to copy quote!", "Quote not generated yet", {
             buttons: false,
             timer: 1500,
             icon: "warning",
-            // className: "black-bg"
         });
     }
 }
@@ -137,7 +100,6 @@ const generate = () => {
             buttons: false,
             timer: 1500,
             icon: "warning",
-            // className: "black-bg"
         });
     }
 }
@@ -146,10 +108,8 @@ const options = {
     method: 'GET',
 	headers: {
 		'X-Api-Key': 'QKjSlqe4GRCy0lDr8BNjRgLKPHxoZLp02kWuwjUx',
-		// 'x-rapidapi-host': 'quotes15.p.rapidapi.com'
 	}
 };
-// document.getElementById('new-quote').addEventListener("click", getQuote(url))
 
 async function getQuote (url) {
     try {
@@ -164,6 +124,3 @@ async function getQuote (url) {
     }
     
 }
-
-
-// getQuote(url);
